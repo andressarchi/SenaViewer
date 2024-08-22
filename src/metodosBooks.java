bf62affc304afbd993a972078af2fcc6d1abcf42
andressarchi <andresfelipesarchivalencia@gmail.com.com>
2024-08-21 22:52:14 -0500

    avance de el menu

--- src/metodosBooks.java
+++ src/metodosBooks.java
@@ -23,9 +23,9 @@ public class metodosBooks {
         String editorial = scanner.nextLine();
 
         System.out.println("Ingrese el ISBN:");
-        int isbn = scanner.nextInt();
+        int isbn = scanner.nextInt(); // Cambiado a String
 
-        Book newBook = new Book(title, editionDate, editorial,isbn);
+        Book newBook = new Book(title, editionDate, editorial, isbn);
         books.add(newBook);
         System.out.println("Libro agregado con éxito");
     }
@@ -35,15 +35,59 @@ public class metodosBooks {
             System.out.println("No se han agregado libros.\nDebes agregar un libro");
             return;
         }
-        System.out.println("\nDetalles de los libros agregados:");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Qué atributo quieres ver?");
+        System.out.println("1. Título");
+        System.out.println("2. Fecha de Edición");
+        System.out.println("3. Editorial");
+        System.out.println("4. ISBN");
+        System.out.println("5. Autores");
+        System.out.println("6. Leído");
+        System.out.println("7. Tiempo Leído");
+        System.out.println("8. ver todo");
+        System.out.print("Ingrese su opción: ");
+        int opcion = scanner.nextInt();
+        scanner.nextLine(); // Consumir el salto de línea pendiente
+
+        System.out.println("\nDetalles de los libros:");
         for (Book book : books) {
-            System.out.println("Título: " + book.getTitle());
-            System.out.println("Fecha de Edición: " + book.getEditionDate());
-            System.out.println("Editorial: " + book.getEditorial());
-            System.out.println("ISBN: " + book.getIsbn());
-            System.out.println("Autores: " + book.getAuthors());
-            System.out.println("Leído: " + book.isReaded());
-            System.out.println("Tiempo Leído: " + book.getTimeReaded() + " minutos");
+            switch (opcion) {
+                case 1:
+                    System.out.println("Título: " + book.getTitle());
+                    break;
+                case 2:
+                    System.out.println("Fecha de Edición: " + book.getEditionDate());
+                    break;
+                case 3:
+                    System.out.println("Editorial: " + book.getEditorial());
+                    break;
+                case 4:
+                    System.out.println("ISBN: " + book.getIsbn());
+                    break;
+                case 5:
+                    System.out.println("Autores: " + book.getAuthors());
+                    break;
+                case 6:
+                    System.out.println("Leído: " + book.isReaded());
+                    break;
+                case 7:
+                    System.out.println("Tiempo Leído: " + book.getTimeReaded() + " minutos");
+                    break;
+                case 8 :
+                    System.out.println("Título: " + book.getTitle());
+                    System.out.println("Fecha de Edición: " + book.getEditionDate());
+                    System.out.println("Editorial: " + book.getEditorial());
+                    System.out.println("ISBN: " + book.getIsbn());
+                    System.out.println("Autores: " + book.getAuthors());
+                    System.out.println("Leído: " + book.isReaded());
+                    System.out.println("Tiempo Leído: " + book.getTimeReaded() + " minutos");
+
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+                    return; // Salir del método si la opción es inválida
+            }
             System.out.println(); // Línea en blanco entre libros
         }
     }

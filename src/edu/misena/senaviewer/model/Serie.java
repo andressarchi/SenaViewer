c9ead7df15cd0b522b9da9a9914d75ded22e2a22
andressarchi <andresfelipesarchivalencia@gmail.com>
2024-09-09 11:42:20 -0500

    se agrega la clase film y se estiende a la main

--- src/edu/misena/senaviewer/model/Serie.java
+++ src/edu/misena/senaviewer/model/Serie.java
@@ -1,51 +1,18 @@
 package edu.misena.senaviewer.model;
 import java.util.Date;
 
-public class Serie {
-    private int id;
-    private String title;
-    private String genre;
-    private String creator;
-    private int duration;
-    private String year;
-    private boolean viewed;
+public class Serie extends Film {
     private int timeViewed;
     private int sessionQuantity;
 
-    public Serie(String title, String genre, int duration) {
-        this.title = title;
-        this.genre = genre;
-        this.duration = duration;
+    public Serie(String title,String genre,String creator,int duration,int timeViewed,int sessionQuantity) {
+        super( title, genre,creator, duration);
+        this.timeViewed = timeViewed;
+        this.sessionQuantity = sessionQuantity;
+
     }
 
-    // Getters
-    public int getId() {
-        return id;
-    }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getCreator() {
-        return creator;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public boolean getViewed() {
-        return viewed;
-    }
 
     public int getTimeViewed() {
         return timeViewed;
@@ -55,34 +22,8 @@ public class Serie {
         return sessionQuantity;
     }
 
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public void setCreator(String creator) {
-        this.creator = creator;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public void setViewed(boolean viewed) {
-        this.viewed = viewed;
-    }
 
     public void setTimeViewed(int timeViewed) {
         this.timeViewed = timeViewed;
